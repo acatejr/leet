0e8d47e1573182f99d7c0684849738f7313bd023
Averill Cate, Jr <acatejr@gmail.com>
2023-12-07 06:56:05 -0700

    Finished number of steps

--- the-leetcode-beginners-guide/src/main.rs
+++ the-leetcode-beginners-guide/src/main.rs
@@ -3,6 +3,7 @@
 /**
  * 
  */
+#[allow(dead_code)]
 fn running_sum(nums: Vec<i32>) -> Vec<i32> {
     let mut results: Vec<i32> = Vec::new();
     results.push(nums[0]);
@@ -17,6 +18,7 @@ fn running_sum(nums: Vec<i32>) -> Vec<i32> {
 /**
  * 
  */
+#[allow(dead_code)]
 fn fizz_buzz(n: i32) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     let p = 4;
@@ -38,10 +40,35 @@ fn fizz_buzz(n: i32) -> Vec<String> {
     return result;
 }
 
+/**
+ * TODO: This can also be done with bitwise operations
+ */
+fn number_of_steps(num: i32) -> i32 {
+    let mut steps = 0;
+    let mut numerator = num;
+
+    if 0 <= num && num <= 1000000 {
+        while numerator > 0 {
+            if numerator % 2 == 0 {
+                numerator /=  2;
+            } else {
+                numerator -= 1;
+            }
+            steps += 1;
+        }
+    }
+    return steps;
+}
+
 fn main() {
-    println!("{:?}", fizz_buzz(3));
-    println!("{:?}", fizz_buzz(5));
-    println!("{:?}", fizz_buzz(15));
+
+    println!("{}", number_of_steps(14));
+    println!("{}", number_of_steps(8));
+    println!("{}", number_of_steps(123));
+    // println!("{:?}", fizz_buzz(3));
+    // println!("{:?}", fizz_buzz(5));
+    // println!("{:?}", fizz_buzz(15));
+
     // let mut nums = vec![1,2,3,4];
     // println!("{:?}", running_sum(nums));
 
