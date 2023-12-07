db9a85a654af8ff7baf4144032ec26ba3521384d
Averill Cate, Jr <acatejr@gmail.com>
2023-12-07 05:56:24 -0700

    Finished fizzbuzz

--- the-leetcode-beginners-guide/src/main.rs
+++ the-leetcode-beginners-guide/src/main.rs
@@ -1,3 +1,8 @@
+#[allow(dead_code)]
+
+/**
+ * 
+ */
 fn running_sum(nums: Vec<i32>) -> Vec<i32> {
     let mut results: Vec<i32> = Vec::new();
     results.push(nums[0]);
@@ -9,13 +14,40 @@ fn running_sum(nums: Vec<i32>) -> Vec<i32> {
     return results;
 }
 
-fn main() {
-    let mut nums = vec![1,2,3,4];
-    println!("{:?}", running_sum(nums));
+/**
+ * 
+ */
+fn fizz_buzz(n: i32) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let p = 4;
 
-    nums = vec![1,1,1,1,1];
-    println!("{:?}", running_sum(nums));
+    if 1 <= n && n <= i32::pow(p, 10) {
+        for i in 1..n+1 {
+            if i % 15 == 0 {
+                result.push("FizzBuzz".to_string());
+            } else if i % 3 == 0 {
+                result.push("Fizz".to_string());
+            } else if i % 5 == 0 {
+                result.push("Buzz".to_string())
+            } else {
+                result.push(i.to_string());
+            }
+        }
+    }
+
+    return result;
+}
+
+fn main() {
+    println!("{:?}", fizz_buzz(3));
+    println!("{:?}", fizz_buzz(5));
+    println!("{:?}", fizz_buzz(15));
+    // let mut nums = vec![1,2,3,4];
+    // println!("{:?}", running_sum(nums));
+
+    // nums = vec![1,1,1,1,1];
+    // println!("{:?}", running_sum(nums));
  
-    nums = vec![3,1,2,10,1];
-    println!("{:?}", running_sum(nums));
+    // nums = vec![3,1,2,10,1];
+    // println!("{:?}", running_sum(nums));
 }
